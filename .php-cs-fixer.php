9f9891926e5cd6e742bec842abae3658d1ec05f7
Chris Holland <cholland@mycertainpath.com>
2024-03-29 23:17:32 -0500

    - tuned php cs fixer - removed phploc dependency

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,6 +8,7 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@PER-CS' => true,
         '@PHP82Migration' => true,
+        'single_line_empty_body' => false,
     ])
     ->setFinder($finder)
 ;
